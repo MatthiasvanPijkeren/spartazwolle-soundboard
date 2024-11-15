76faaf5efbf2cff01838a7b689cc9bf5b3245a7e
Matthias van Pijkeren <matthias@pixelpillow.nl>
2024-11-15 12:38:19 +0100

    Added fade

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,33 +1,56 @@
 import { Howl } from "howler";
 
 const soundPlayer = {
-    currentSound: null, // Tracks the currently playing sound
+    currentSound: null,
 
-    playSound(soundFile) {
+    playSound(soundFile, callback) {
+        if (this.currentSound) {
+            // Stop the current sound if a different one is selected
+            this.currentSound.stop();
+        }
+
+        this.currentSound = new Howl({
+            src: [soundFile],
+            html5: true,
+            volume: 1.0, // Start at full volume
+            onend: () => {
+                console.log(`Finished playing: ${soundFile}`);
+                callback(false); // Notify Alpine.js playback has stopped
+            },
+        });
+
+        this.currentSound.play();
+        callback(true); // Notify Alpine.js playback has started
+    },
+
+    fadeOutSound(callback) {
+        if (this.currentSound && this.currentSound.playing()) {
+            // Fade out over 1 second (1000 ms)
+            this.currentSound.fade(this.currentSound.volume(), 0, 1000);
+
+            // After fade-out is complete, stop the sound and reset state
+            setTimeout(() => {
+                this.currentSound.stop();
+                callback(false); // Notify Alpine.js playback has stopped
+            }, 1000); // Match the fade duration
+        }
+    },
+
+    toggleSound(soundFile, callback) {
         if (this.currentSound && this.currentSound._src === soundFile) {
+            // Toggle fade-out/pause if the same sound is selected
             if (this.currentSound.playing()) {
-                this.currentSound.pause();
-                console.log("Sound paused.");
+                this.fadeOutSound(callback);
             } else {
                 this.currentSound.play();
-                console.log("Sound resumed.");
+                callback(true);
             }
         } else {
-            if (this.currentSound) {
-                this.currentSound.stop();
-            }
-
-            this.currentSound = new Howl({
-                src: [soundFile],
-                html5: true,
-                onplay: () => console.log(`Playing: ${soundFile}`),
-                onend: () => console.log(`Finished playing: ${soundFile}`),
-            });
-
-            this.currentSound.play();
+            // Play a new sound
+            this.playSound(soundFile, callback);
         }
     },
 };
 
-// Make soundPlayer globally accessible
+// Make the soundPlayer globally accessible
 window.soundPlayer = soundPlayer;
